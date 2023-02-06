391a2a2a0cc56b2fede37a55c85fac66e310a38e
sebastian jurs <sebastian@jursrasmussen.dk>
2023-02-06 09:30:46 +0100

    færdig med opgave 3

--- øvelser/e03_multiplication/src/multiplication.py
+++ øvelser/e03_multiplication/src/multiplication.py
@@ -2,7 +2,8 @@
 
 def main():
     # Enter your solution here
-    print("")
-
+    for i in range(11): 
+        print("4 multiplied by", i, "is", i * 4)
+        
 if __name__ == "__main__":
     main()
