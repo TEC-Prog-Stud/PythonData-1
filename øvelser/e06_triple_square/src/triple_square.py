1b46d321566c6780b9fbef3db2b1f6956721dda1
sebastian jurs <sebastian@jursrasmussen.dk>
2023-02-06 12:39:59 +0100

    Færdig med opgav e 7

--- øvelser/e06_triple_square/src/triple_square.py
+++ øvelser/e06_triple_square/src/triple_square.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
 
 
+def triple(t):
+    "This is multi with 3"
+    t = t* 3
+    return(t)
+
+
+def square(s):
+    "This use power of 2"
+    s = s **2
+    return(s)
+
 def main():
-    pass
+    for i in range(1,11):
+        ts = triple(i)    
+        st = square(i)
+        if ts < st:
+            break
+        else:
+            print(f"triple({i})=={ts}", f"square({i})=={st}")
 
 if __name__ == "__main__":
     main()
--- øvelser/e07_areas_of_shapes/src/areas_of_shapes.py
+++ øvelser/e07_areas_of_shapes/src/areas_of_shapes.py
@@ -4,7 +4,32 @@ import math
 
 
 def main():
-    # enter you solution here
+
+    loop = True
+    while loop:
+        shape = input("Enter shape(triangle/rectangle/circle): ")
+        if shape == "":
+            loop = False
+        elif shape == "triangle":
+            base = float(input("Enter base: "))
+            height = float(input("Enter height: "))
+            result = 0.5 * base * height
+            if result:
+                print("The area is {:.6f}".format(result))
+        elif shape == "rectangle":
+            width = float(input("Enter width: "))
+            height = float(input("Enter height: "))
+            result = width * height
+            if result:
+                print("The area is {:.6f}".format(result))
+        elif shape == "circle":
+            radius = float(input("Enter radius: "))
+            result = math.pi * radius ** 2
+            if result:
+                print("The area is {:.6f}".format(result))
+        else:
+            print("Unknown shape!")
+
 
 if __name__ == "__main__":
     main()
