e288996585ea1282ccad374e3163285e7e3f9340
sebastian jurs <sebastian@jursrasmussen.dk>
2023-02-06 10:15:59 +0100

    færdig med opgave 4

--- øvelser/e04_multiplication_table/src/multiplication_table.py
+++ øvelser/e04_multiplication_table/src/multiplication_table.py
@@ -2,7 +2,19 @@
 
 
 def main():
-    pass
+    for i in range(1):
+        for i in range(1, 11):
+            print((i*1),
+                  (i*2),
+                  (i*3),
+                  (i*4),
+                  (i*5),
+                  (i*6),
+                  (i*7),
+                  (i*8),
+                  (i*9),
+                  (i*10),
+                  )
 
 if __name__ == "__main__":
     main()
