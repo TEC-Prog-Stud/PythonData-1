7a24bf70e654c29e241ab32be55c562cde28855e
sebastian jurs <sebastian@jursrasmussen.dk>
2023-02-07 13:09:37 +0100

    Færdig med opgave 16

--- øvelser/e16_transform/src/transform.py
+++ øvelser/e16_transform/src/transform.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
 def transform(s1, s2):
-    return []
+    l1 = list(map(int, s1.split()))
+    l2 = list(map(int, s2.split()))
+    
+    # z = list(zip(s1.split(), s2.split()))
+    # L = list(map(lambda t : int(t[0]) * int(t[1]), z))
+    
+    return [x*y for x, y in zip(l1, l2)]
 
 def main():
-    pass
+    print(transform("1 5 3", "2 6 -1"))
 
 if __name__ == "__main__":
     main()
