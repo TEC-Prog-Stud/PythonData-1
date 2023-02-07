0b9d12636abd3090c1c6511929c40495fb885a8d
sebastian jurs <sebastian@jursrasmussen.dk>
2023-02-07 08:18:44 +0100

    færsig med opgave 8

--- øvelser/e08_solve_quadratic/src/solve_quadratic.py
+++ øvelser/e08_solve_quadratic/src/solve_quadratic.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 
+
 import math
+import numpy as np
 
 def solve_quadratic(a, b, c):
-    return (0,0)
+  d = math.sqrt(b ** 2 - 4 * a * c)
+  sol1 = (-b + d) / (2 * a)
+  sol2 = (-b - d) / (2 * a)
+  return (sol1, sol2)
 
 
 def main():
-    pass
+
+    print(solve_quadratic(1, -3, 2))
+    print(solve_quadratic(1, 2, 1))
 
 if __name__ == "__main__":
     main()
