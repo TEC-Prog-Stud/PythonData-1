f40c0bea7d1e1404a0660dedff9cca148471365e
sebastian jurs <sebastian@jursrasmussen.dk>
2023-02-07 12:34:16 +0100

    færdig med opgave 14

--- øvelser/e14_find_matching/src/find_matching.py
+++ øvelser/e14_find_matching/src/find_matching.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
-def find_matching(L, pattern):
-    return []
+def find_matching(List, pattern):
+    return [
+        i 
+        for i, string in enumerate(List) 
+        if pattern in string
+        ]
+
 
 def main():
-    pass
+    print(find_matching(["sensitive", "engine", "rubbish", "comment"], "i"))
 
 if __name__ == "__main__":
     main()
