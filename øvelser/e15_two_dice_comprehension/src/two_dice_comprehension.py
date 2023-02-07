0d2b224a6b9a1cb62694652d4a18a8fcb1820a46
sebastian jurs <sebastian@jursrasmussen.dk>
2023-02-07 12:46:04 +0100

    færdig med opgave 15

--- øvelser/e15_two_dice_comprehension/src/two_dice_comprehension.py
+++ øvelser/e15_two_dice_comprehension/src/two_dice_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def main():
-    pass
-
+    combinations = [(d1, d2) for d1 in range(1, 7) for d2 in range(1, 7) if d1 + d2 == 5]
+    # for combination in combinations:
+    #     print(combination)
+    print(*combinations, sep="\n")
 if __name__ == "__main__":
     main()
