5b50d86d952ae3f89e634db82975bed181957ede
sebastian jurs <sebastian@jursrasmussen.dk>
2023-02-07 10:25:26 +0100

    opgave 10

--- øvelser/e10_detect_ranges/src/detect_ranges.py
+++ øvelser/e10_detect_ranges/src/detect_ranges.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 
-def detect_ranges(L):
-    return []
+def detect_ranges(lst):
+  lst = sorted(lst)
+  result = []
+  start = lst[0]
+  end = start
+  for i in range(1, len(lst)):
+    if lst[i] == lst[i-1] + 1:
+      end = lst[i]
+    else:
+      if start == end:
+        result.append(start)
+      else:
+        result.append((start, end+1))
+      start = lst[i]
+      end = start
+  if start == end:
+    result.append(start)
+  else:
+    result.append((start, end+1))
+  return result
 
 def main():
     L = [2, 5, 4, 8, 12, 6, 7, 10, 13]
