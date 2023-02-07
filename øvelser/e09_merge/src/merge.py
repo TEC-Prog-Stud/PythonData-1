ba202bf0514c12c76f5d56308f47f85ba6c0e41a
sebastian jurs <sebastian@jursrasmussen.dk>
2023-02-07 08:42:59 +0100

    Færdig med opgave 9

--- øvelser/e09_merge/src/merge.py
+++ øvelser/e09_merge/src/merge.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 
 def merge(L1, L2):
-    return []
+    result = []
+    i, j = 0, 0
+    while i < len(L1) and j < len(L2):
+        if L1[i] < L2[j]:
+            result.append(L1[i])
+            i += 1
+        else:
+            result.append(L2[j])
+            j += 1
+    result += L1[i:]
+    result += L2[j:]
+    return result
+
 
 def main():
-    pass
+    L1 = [1, 3, 5, 7]
+    L2 = [2, 4, 6, 8]
+    L = merge(L1, L2)
+    print(L)  # [1, 2, 3, 4, 5, 6, 7, 8]
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
