af541657b2c5966c33234a41dbc13a9487b72fdb
sebastian jurs <sebastian@jursrasmussen.dk>
2023-02-08 10:37:52 +0100

    prøver stadig med opgave 24

--- øvelser/e26_file_count/src/file_count.py
+++ øvelser/e26_file_count/src/file_count.py
@@ -3,10 +3,21 @@
 import sys
 
 def file_count(filename):
-    return (0, 0, 0)
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        linecount = len(lines)
+        wordcount = 0
+        charcount = 0
+        for line in lines:
+            words = line.split()
+            wordcount += len(words)
+            charcount += len(line)
+        return (linecount, wordcount, charcount)
 
 def main():
-    pass
+    for filename in sys.argv[1:]:
+        linecount, wordcount, charcount = file_count(filename)
+        print(linecount, '\t', wordcount, '\t', charcount, '\t', filename)
 
 if __name__ == "__main__":
     main()
--- øvelser/e27_file_extensions/src/file_extensions.py
+++ øvelser/e27_file_extensions/src/file_extensions.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
 
 def file_extensions(filename):
-    return ([], {})
+    no_extension = []
+    extensions = {}
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.strip()
+            index = line.rfind('.')
+            if index == -1:
+                no_extension.append(line)
+            else:
+                ext = line[index + 1:]
+                if ext in extensions:
+                    extensions[ext].append(line)
+                else:
+                    extensions[ext] = [line]
+    return (no_extension, extensions)
 
 def main():
-    pass
+    no_extension, extensions = file_extensions('src/filenames.txt')
+    print(len(no_extension), 'files with no extension')
+    for ext in sorted(extensions.keys()):
+        print(ext, len(extensions[ext]))
 
 if __name__ == "__main__":
     main()
