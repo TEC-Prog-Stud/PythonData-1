418e605840979dc27e20fd529ea99c42cf806a04
sebastian jurs <sebastian@jursrasmussen.dk>
2023-02-08 10:30:15 +0100

    i gang med opgav 24

--- øvelser/e24_word_frequencies/src/word_frequencies.py
+++ øvelser/e24_word_frequencies/src/word_frequencies.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
+import string
+
 def word_frequencies(filename):
     with open(filename, "r") as f:
         data = f.read()
-    # split the data into words
     words = data.split()
-    # remove punctuation from the ends of the words
     words = [word.strip(string.punctuation) for word in words]
-    # count the frequency of each word
     word_freq = {}
     for word in words:
         if word in word_freq:
@@ -18,7 +17,6 @@ def word_frequencies(filename):
 
 def main():
     freq = word_frequencies("alice.txt")
-    # sort the dictionary by frequency and print the results
     for word, count in sorted(freq.items(), key=lambda x: x[1], reverse=True):
         print(f"{word}\t{count}")
 
--- øvelser/e25_summary/src/summary.py
+++ øvelser/e25_summary/src/summary.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python3
 
+import statistics
 import sys
 
+
 def summary(filename):
-    return (0,0,0)
+    numbers = []
+    with open(filename, 'r') as f:
+        for line in f:
+            try:
+                x = float(line.strip())
+                numbers.append(x)
+            except ValueError:
+                # ignore lines that can't be converted to float
+                pass
+    if not numbers:
+        return
+    avg = statistics.mean(numbers)
+    stddev = statistics.pstdev(numbers, avg)
+    return sum(numbers), avg, stddev
+
 
 def main():
-    pass
+    for filename in sys.argv[1:]:
+        res = summary(filename)
+        if res:
+            sum_, avg, stddev = res
+            print(
+                f'File: {filename} Sum: {sum_:.6f} Average: {avg:.6f} Stddev: {stddev:.6f}')
 
 if __name__ == "__main__":
     main()
