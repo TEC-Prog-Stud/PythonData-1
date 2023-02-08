963a9245f66bb8e930f1da785daace8d8a8d1fb1
Daniel Hartwich <dhartwich650@gmail.com>
2023-02-08 13:08:06 +0100

    did assignment 21-26

--- øvelser/e22_file_listing/src/file_listing.py
+++ øvelser/e22_file_listing/src/file_listing.py
@@ -4,10 +4,29 @@ import re
 
 
 def file_listing(filename="src/listing.txt"):
-    return []
+    result = []
+    with open(filename, "r") as f:
+        for line in f:
+            regex = r"\s+(\d+)\s([A-Za-z]{3})\s+(\d+)\s(\d)+:(\d+)\s([-_.A-Za-z \d]+)"
+            res = re.search(regex, line)
+            if res:
+                print(res.groups())    
+                g = res.groups()
+                t = (
+                    int(g[0]),
+                    g[1],
+                    int(g[2]),
+                    int(g[3]),
+                    int(g[4]),
+                    g[5]
+                )
+                print(t)
+                result.append(t)
+    return result
 
 def main():
-    pass
+    print(file_listing(r'øvelser\e22_file_listing\src\listing.txt'))
+
 
 if __name__ == "__main__":
     main()
--- øvelser/e23_red_green_blue/src/red_green_blue.py
+++ øvelser/e23_red_green_blue/src/red_green_blue.py
@@ -2,12 +2,25 @@
 
 import re
 
+
 def red_green_blue(filename="src/rgb.txt"):
-    return []
+    L = []
+    lines = []
+    with open(filename, 'r') as f:
+        for l in f:
+            rx = r'^\s*(\d+)\s*(\d+)\s*(\d+)\s*(.*?)\n$'
+            r = re.search(rx, l)
+            if r:
+                g = r.groups()
+                m = (g[0] + '\t')
+                m = '\t'.join(g)
+                L.append(m)
+    return L
 
 
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
