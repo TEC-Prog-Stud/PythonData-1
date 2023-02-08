b9a636cb1d2c472f7a4dc8fb5f84b4ef47973e33
sebastian jurs <sebastian@jursrasmussen.dk>
2023-02-08 10:27:26 +0100

    Færdig med opgave 23

--- øvelser/e24_word_frequencies/src/word_frequencies.py
+++ øvelser/e24_word_frequencies/src/word_frequencies.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 
 def word_frequencies(filename):
-    return {}
+    with open(filename, "r") as f:
+        data = f.read()
+    # split the data into words
+    words = data.split()
+    # remove punctuation from the ends of the words
+    words = [word.strip(string.punctuation) for word in words]
+    # count the frequency of each word
+    word_freq = {}
+    for word in words:
+        if word in word_freq:
+            word_freq[word] += 1
+        else:
+            word_freq[word] = 1
+    return word_freq
 
 def main():
-    pass
+    freq = word_frequencies("alice.txt")
+    # sort the dictionary by frequency and print the results
+    for word, count in sorted(freq.items(), key=lambda x: x[1], reverse=True):
+        print(f"{word}\t{count}")
 
 if __name__ == "__main__":
     main()
