d67596d5b4542d510e666b4ae5d85149f81a4399
sebastian jurs <sebastian@jursrasmussen.dk>
2023-02-08 09:11:30 +0100

    Begyndt på opgave 22

--- øvelser/e22_file_listing/src/file_listing.py
+++ øvelser/e22_file_listing/src/file_listing.py
@@ -4,8 +4,16 @@ import re
 
 
 def file_listing(filename="src/listing.txt"):
-    return []
-
+    result = []
+    with open(filename, 'r') as file:
+        for line in file:
+            match = re.search(
+                r'(\d+)\s+(\w+)\s+(\d+)\s+(\d+):(\d+)\s+(.+)', line)
+            if match:
+                size, month, day, hour, minute, filename = match.groups()
+                result.append((int(size), month, int(day),
+                              int(hour), int(minute), filename))
+    return result
 def main():
     pass
 
