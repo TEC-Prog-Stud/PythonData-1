81a259c7262efd5dc3c352c0e63d79deb8d807b6
sebastian jurs <sebastian@jursrasmussen.dk>
2023-02-08 09:36:14 +0100

    Færdig med opgave 22

--- øvelser/e23_red_green_blue/src/red_green_blue.py
+++ øvelser/e23_red_green_blue/src/red_green_blue.py
@@ -3,11 +3,23 @@
 import re
 
 def red_green_blue(filename="src/rgb.txt"):
-    return []
-
+    with open("src/rgb.txt", "r") as f:
+        data = f.readlines()
+    # remove the first line
+    data = data[1:]
+    cleaned_data = []
+    # use regular expressions to clean up the data
+    for line in data:
+        match = re.search(r"(\d+)\s+(\d+)\s+(\d+)\s+(.+)", line)
+        red = match.group(1)
+        green = match.group(2)
+        blue = match.group(3)
+        name = match.group(4)
+        cleaned_data.append(f"{red}\t{green}\t{blue}\t{name}")
+    return cleaned_data
 
 def main():
-    pass
+    print(red_green_blue)
 
 if __name__ == "__main__":
     main()
