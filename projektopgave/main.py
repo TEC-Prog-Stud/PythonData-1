46bf1c77672219d0a285dfc7437488e5b3f9cb14
Daniel Hartwich <dhartwich650@gmail.com>
2023-02-10 10:21:54 +0100

    Writing comments

--- projektopgave/main.py
+++ projektopgave/main.py
@@ -1,3 +1,4 @@
+#importing modules
 import csv
 import datetime
 from fit_file import read
@@ -11,43 +12,20 @@ def pace_to_velocity(pace):
     return v
 
 
-def read_csv(points):
-    # regexTime = r'\s(\d+):(\d+):(\d+)'
-    time = []
-    l0 = 0
-    l1 = 1
-    with open('projektopgave\data\hok_klubmesterskab_2022\kontroltider.csv', 'r',
-              encoding='utf-8', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        postkontroller = [{'nr': row['nr'],
-                       'timestamp': datetime.datetime.fromisoformat(row['timestamp'])}
-                      for row in reader]
-    while l1 < len(postkontroller):
-        t = postkontroller[l1]['timestamp'] - postkontroller[l0]['timestamp']
-        l0 += 1
-        l1 += 1
-        # print(t)
-        time.append(t)
-        
-    st_1 = [p for p in points if p['timestamp'].astimezone() < postkontroller[1]['timestamp'] ]
-    # her ses postkontrollens timestamp, og det sidste punkt fra fit filen der er før posten
-    postkontroller[1]['timestamp'], st_1[-1]
-    b = 0
-    for a in st_1:
-        # print(f"(Taler: {b}, Latitude: {a['latitude']}, Longtitude {a['longitude']}, TimeStamp: {a['timestamp']})")
-        b += 1
-    # print(st_1)
-
-
-
+# this is a function to get all the data from the file and making it easier to read
 def distance_over_time_between_two_points(points):
+
     lines = []
     for i, p in enumerate(points[1:]):
-    # bemærk at i starter på 0 selv om vi slicer med [1:]
-        pp = points[i]                # previous_point
+            # Get previous point
+        pp = points[i]
+            # Calculate time difference     
         dt = (p['timestamp'] - pp['timestamp']).seconds
+            # Calculate distance
         dd = distance( (pp['latitude'], pp['longitude']), (p['latitude'], p['longitude'])).meters
+            # Calculate velocity
         v = dd/dt
+            # Store data in line dictionary
         line = {
             'start'             : pp,
             'end'               : p,
@@ -56,8 +34,10 @@ def distance_over_time_between_two_points(points):
             'veloicty'          : v,
         }
         lines.append(line)
+        # Print results
     print(lines)
 
+    # Variables
     walkTime = 0
     runTime = 0
     idleTime = 0
@@ -66,49 +46,54 @@ def distance_over_time_between_two_points(points):
     idleDistance = 0
     totalTime = 0
     totaldistance = 0
-    totalWalk = []
-    totalRun = []
-    totalIdle = []
+    # for loop
     for a in lines:
-        # print(f"Delta Time {a['delta_time']}, Delta Distance {a['delta_distance']}, Veloicty {a['veloicty']}")
+        # if statements for what his velocity is
+        #this is idle
         if a['veloicty'] < 0.33333:
+            # takes the varibale idleTime  and adds delta_time to it
             idleTime += a['delta_time']
+            # takes the varibale idleDistance and adds delta_distance to it
             idleDistance += a['delta_distance']
             print("står stille","\n", "deltatime: ",idleTime, "deltadistance:",idleDistance)
-            totalIdle.append((idleTime, idleDistance))
+        #this is walk
         elif a['veloicty'] < 1.666667:
             walkTime += a['delta_time']
             walkDistance += a['delta_distance']
             print("Går","\n", "deltatime: ",walkTime, "deltadistance:",walkDistance)
-            totalWalk.append((walkTime, walkDistance))
+        # this is run
         else:
             runTime += a['delta_time']
             runDistance += a['delta_distance']
             print("Løber","\n", "deltatime: ",runTime, "deltadistance:",runDistance)
-            totalRun.append((runTime, runDistance))
 
         totalTime += a['delta_time']
         totaldistance += a['delta_distance']
     print(f"\n\nTotal Time {totalTime}, Total Distance {totaldistance}")
 
+    # formates the info like the assignment said, and adding some of the variables as procent 
     print(f'\n\n\tmeters, seconds, % of time')
     print(f'Run:\t{runDistance:.1f}\t{runTime}\t{runTime/totalTime:.1%}')
     print(f'Walk:\t{walkDistance:.1f}\t{walkTime}\t{walkTime/totalTime:.1%}')
     print(f'Stand:\t{idleDistance:.1f}\t{idleTime}\t{idleTime/totalTime:.1%}')
-    print(f'Total:\t{totaldistance:.1f}\t{totalTime}\t{totalTime/totalTime:.1%}')
+    print(f'Total:\t{totaldistance:.1f}\t{totalTime}\t{totalTime/totalTime:.0%}')
 
 
 
 def main():
+    #variable with the relative file path to a .FIT file
     fname = "projektopgave\data\hok_klubmesterskab_2022\CA8D1347.FIT"
+    #Reading the FIT file
     points = read.read_points(fname)
-    # print(len(points))
-    # print(points[300])
+    #priting the length of points
+    print(len(points))
+    #printing the number 300 of the list of points
+    print(points[300])
+
+    ## calling my functions
     print(pace_to_velocity(10))
-    # read_csv(points)
     distance_over_time_between_two_points(points)
-    pass
 
-
+# if name is main execute main function
 if __name__ == "__main__":
     main()
