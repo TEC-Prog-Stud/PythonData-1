9c13574c90117d761ac8249cbe7fb6f41907e1ba
Zerraphix <joey.knutsson@rocketmail.com>
2023-06-02 10:33:25 +0200

    fbght

--- projektopgave/handout.py
+++ projektopgave/handout.py
@@ -20,5 +20,39 @@ def indlaes_fra_fit(fname = "data/hok_klubmesterskab_2022/CA8D1347.FIT"):
 
 punkter = indlaes_fra_fit()
 
+distancetime = {}
+
 print(f"Der er indlæst {len(punkter)} punkter fra filen")
 print(punkter[300])
+
+## -----------------------------------------------------------------
+
+from geopy.distance import distance 
+
+# afstand mellem punkter
+
+# Ved at bruge enumarate får jeg både hvert element (`p`), og et index for hvert element (`i`), altid startende med 0 
+# Jeg itereret over listen `punkter[1:]` hvor første element er slicet væk
+#   inde i løkken henter jeg forrige punkt med index'et i 
+#     Da i starter på nul, for jeg hvert punkt fra 0 til nedstsidste,
+#     punktet p er hvert af punkterne fra andet til sidste
+def DistanceUdregning(punkter):
+    for i, p in enumerate(punkter[1:]):
+        # previus point
+        pp = punkter[i]
+
+        dt = (p['timestamp'] - pp['timestamp']).seconds
+        dd = distance( (pp['latitude'], pp['longitude']), (p['latitude'], p['longitude'])).meters
+        v = dd/dt
+        #print(f"p: {p}")
+        #print(f"pp: {pp}")
+        #print(f"dt: {dt}")
+        #print(f"dd: {dd}")
+
+        #distancetime.append([dt,dd])
+        distancetime[dd] = distancetime.get(dd, dt)
+
+DistanceUdregning(punkter)
+for distance, time in distancetime.items():
+        print(f"Afstand: {distance: .1f}\t Tid: {time}")
+#print(distancetime)
